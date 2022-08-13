aed291a51bb378e01c79609fa78cbc9a92d4d104
Pantelis Eleftheriadis <pantelis.eleftheriadis@optima-apps.com>
2022-08-13 14:33:16 +0300

    Adding menu animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import '@styles/locomotive.scss';
 import type { AppProps } from 'next/app';
 import LocomotiveLayout from '@layouts/LocomotiveLayout';
 import Footer from '@components/Footer';
+import AppMenu from '@components/AppMenu';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <LocomotiveLayout>
+            <AppMenu />
             <Component {...pageProps} />
             <Footer />
         </LocomotiveLayout>
