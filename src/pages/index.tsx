233a4f18eb261ac8ed1b23d6a5d124278b163081
pantelis.eleftheriadis <pantelis.eleftheriadis@optima-apps.com>
2022-08-31 17:16:47 +0200

    Added grid of clients

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next';
 import Wonders from '@modules/home/Wonders';
 import Features from '@modules/home/Features';
+import Clients from '@modules/home/Clients';
 
 const Home: NextPage = () => {
     return (
         <>
             <Wonders />
             <Features />
+            <Clients />
         </>
     );
 };
