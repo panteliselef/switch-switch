a4d781a30a71920200c26b6e09429e2f3b1fca44
Pantelis Eleftheriadis <pantelis.eleftheriadis@optima-apps.com>
2022-09-04 20:13:09 +0200

    Added contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Contact from '@modules/contact';
+
+const ContactPage: NextPage = () => {
+    return (
+        <>
+            <Contact />
+        </>
+    );
+};
+
+export default ContactPage;
