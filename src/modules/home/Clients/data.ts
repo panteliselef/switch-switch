0ad6bd8b2b89d087a45abc40b857e315a87a3cac
Pantelis Eleftheriadis <pantelis.eleftheriadis@optima-apps.com>
2022-09-14 09:33:11 +0200

    Updating list of clients

--- src/modules/home/Clients/data.ts
+++ src/modules/home/Clients/data.ts
@@ -22,26 +22,11 @@ export const clients = [
         url: 'http://1102designspace.com/?i=1',
     },
     {
-        logo: al,
-        name: 'Aleksandar Vujinović',
-        text: '',
-        url: 'https://www.instagram.com/aleksandar.vujnovic/?hl=en',
-    },
-
-    {
         logo: Ana2,
         name: 'Ana Nenadović',
         text: 'Naša misija je transformacija prostora u fuziju umetnosti i funkcionalnosti prema meri svakog korisnika. Uz entuzijazam i posvećenost, bez strahova i predrasuda, pratimo Vaše želje i pretačemo ih u realnost bilo da se radi o stambenim ili komercijalnim ambijentima. Studio se nalazi u Beogradu. Osnovan je 2013.godine sa željom da ostavi pečat kako u svetu umetnosti tako i u srcima ljudi.',
         url: 'https://ananenadovic.com/sr/',
     },
-
-    {
-        logo: Dizajn,
-        name: 'Ana Tanasković',
-        text: '',
-        url: 'https://ananenadovic.com/sr/',
-    },
-
     {
         logo: Archit,
         name: 'Archit Studio',
@@ -65,13 +50,6 @@ We are minimalists with a touch of luxurious spirit.
     },
 
     {
-        logo: Bojan,
-        name: 'Bojan Pavlović',
-        text: ``,
-        url: 'https://www.instagram.com/bolebole_arh/?hl=en',
-    },
-
-    {
         logo: Branko,
         name: 'Branko Burmaz',
         text: `Arhitektonski biro orjentisan pretežno ka objektima za stanovanje, odmor i ugostiteljstvo. Sa velikim iskustvom i obimnim radom u dizajnu enterijera.`,
@@ -86,7 +64,6 @@ We work on planning, interior design and furnishing projects.
 What make us special is passion for high aesthetic values.`,
         url: 'http://cattarohome.com/',
     },
-
     {
         logo: ENT,
         name: 'Ent Miller',
@@ -94,80 +71,6 @@ What make us special is passion for high aesthetic values.`,
 
 Svojim klijentima obezbeđujemo pre svega profesionalan i kvalitetan pristup u izvođenju projekata, svesni odgovornosti koju za nas svaki projekat nosi. Našim poslovnim poduhvatima pristupamo ozbiljno, temeljno i agilno, svesni da je naša misija, kao nekoga ko ostavlja trag i pečat u urbanom prostoru, veoma zahtevna i značajna.`,
         url: 'https://mmccap.com/entmiller.com/index.html\n',
-    },
-
-    {
-        logo: '',
-        name: 'G Studio',
-        text: ``,
-        url: 'https://www.instagram.com/studiog.rs/',
-    },
-
-    {
-        logo: '',
-        name: 'Jelena Jakovljević',
-        text: ``,
-        url: 'https://www.instagram.com/jenajakovljevich/',
-    },
-
-    {
-        logo: '',
-        name: 'Jelena Trimčević',
-        text: ``,
-        url: 'https://www.instagram.com/jelenatrimcevic/',
-    },
-
-    {
-        logo: '',
-        name: 'Max Company',
-        text: `MAX company čini tim inženjera i dizajnera sa višegodišnjim iskustvom u projektovanju, nadzoru i izvodjenju projekata raznih namena od Srbije, preko Zapadne Evrope do Bliskog istoka.
-Specijalizovali smo se za dizajn, projektovanje i opremanje svih vrsta enterijera, od stanova i apartmana, preko poslovnih prostora do hotela i trgovačkih centara.
-Iza nas su na stotine izvedenih projekata i isto toliko zadovoljnih klijenata.`,
-        url: 'https://www.instagram.com/maxcompany.rs/?hl=en',
-    },
-
-    {
-        logo: '',
-        name: 'Monika Štiklica',
-        text: `Monika Štiklica, diplomirani arhitekta unutrašnje arhitekture i doktor tehničkih nauka, sa svojim saradnicima, nudi potpun servis u pogledu dizajna enterijera rezidencijalnih i javnih objekata, pružajući klijentima usluge projektovanja enterijera, odabira arhitektonskih detalja i finitura, sarađujući sa izvođačima obezbeđuje autentičnost finalnog izgleda.`,
-        url: 'https://www.monikastiklica.com/\n',
-    },
-
-    {
-        logo: '',
-        name: 'MVS',
-        text: ``,
-        url: 'https://www.instagram.com/mvs.project',
-    },
-
-    {
-        logo: nk,
-        name: 'NK Arch Design',
-        text: `NK ARCHITECTS design is grounded in a simple philosophy, but one that has infinate power: "To bring the imagination to life"`,
-        url: 'https://www.instagram.com/nk_arch_design/?hl=en',
-    },
-
-    {
-        logo: '',
-        name: 'No Mad Design',
-        text: ``,
-        url: 'http://www.nomad-design.rs/',
-    },
-
-    {
-        logo: '',
-        name: 'Parapet Studio',
-        text: `Stvaramo jedinstveni prostor koje inspiriše, maksimalno koristeći potencijal enterijera.`,
-        url: 'https://www.instagram.com/parapet.studio/?hl=en',
-    },
-
-    {
-        logo: '',
-        name: 'Studio Loft',
-        text: `Dobar dizajn neće spasiti svet, ali sasvim sigurno će ga učiniti lepšim!
-
-Naš inkluzivni pristup čini Vas partnerom u procesu, pretvarajući pojedinačne ideje u zajedničku viziju I transformišući prostore prema Vašim ličnim potrebama, prožetim iskustvom naših stručnjaka.`,
-        url: 'https://studioloft.rs',
     },
 
     {
@@ -199,12 +102,105 @@ Naš inkluzivni pristup čini Vas partnerom u procesu, pretvarajući pojedinačn
     },
 
     {
+        logo: nk,
+        name: 'NK Arch Design',
+        text: `NK ARCHITECTS design is grounded in a simple philosophy, but one that has infinate power: "To bring the imagination to life"`,
+        url: 'https://www.instagram.com/nk_arch_design/?hl=en',
+    },
+
+    {
+        logo: al,
+        name: 'Aleksandar Vujinović',
+        text: '',
+        url: 'https://www.instagram.com/aleksandar.vujnovic/?hl=en',
+    },
+
+    {
+        logo: Dizajn,
+        name: 'Ana Tanasković',
+        text: '',
+        url: 'https://ananenadovic.com/sr/',
+    },
+
+    {
+        logo: Bojan,
+        name: 'Bojan Pavlović',
+        text: ``,
+        url: 'https://www.instagram.com/bolebole_arh/?hl=en',
+    },
+    {
+        logo: '',
+        name: 'Parapet Studio',
+        text: `Stvaramo jedinstveni prostor koje inspiriše, maksimalno koristeći potencijal enterijera.`,
+        url: 'https://www.instagram.com/parapet.studio/?hl=en',
+    },
+
+    {
+        logo: '',
+        name: 'Max Company',
+        text: `MAX company čini tim inženjera i dizajnera sa višegodišnjim iskustvom u projektovanju, nadzoru i izvodjenju projekata raznih namena od Srbije, preko Zapadne Evrope do Bliskog istoka.
+Specijalizovali smo se za dizajn, projektovanje i opremanje svih vrsta enterijera, od stanova i apartmana, preko poslovnih prostora do hotela i trgovačkih centara.
+Iza nas su na stotine izvedenih projekata i isto toliko zadovoljnih klijenata.`,
+        url: 'https://www.instagram.com/maxcompany.rs/?hl=en',
+    },
+
+    {
+        logo: '',
+        name: 'Monika Štiklica',
+        text: `Monika Štiklica, diplomirani arhitekta unutrašnje arhitekture i doktor tehničkih nauka, sa svojim saradnicima, nudi potpun servis u pogledu dizajna enterijera rezidencijalnih i javnih objekata, pružajući klijentima usluge projektovanja enterijera, odabira arhitektonskih detalja i finitura, sarađujući sa izvođačima obezbeđuje autentičnost finalnog izgleda.`,
+        url: 'https://www.monikastiklica.com/\n',
+    },
+
+    {
+        logo: '',
+        name: 'Studio Loft',
+        text: `Dobar dizajn neće spasiti svet, ali sasvim sigurno će ga učiniti lepšim!
+
+Naš inkluzivni pristup čini Vas partnerom u procesu, pretvarajući pojedinačne ideje u zajedničku viziju I transformišući prostore prema Vašim ličnim potrebama, prožetim iskustvom naših stručnjaka.`,
+        url: 'https://studioloft.rs',
+    },
+
+    {
         logo: '',
         name: 'Studio Nacrt',
         text: `STUDIO NACRT is a group of highly educated interior and furniture designers with rich experience, who specialize in 3d modeling, 3d visualization and designing of interiors and furniture. While designing, we specially emphasize the role of details and carefully selected materials. We believe in the power of materials like stone, concrete, wood and glass, whose unique character defines the entire space that defies stereotypes.`,
         url: 'https://studionacrt.com/?lang=en',
     },
 
+    {
+        logo: '',
+        name: 'MVS',
+        text: ``,
+        url: 'https://www.instagram.com/mvs.project',
+    },
+
+    {
+        logo: '',
+        name: 'No Mad Design',
+        text: ``,
+        url: 'http://www.nomad-design.rs/',
+    },
+
+    {
+        logo: '',
+        name: 'G Studio',
+        text: ``,
+        url: 'https://www.instagram.com/studiog.rs/',
+    },
+
+    {
+        logo: '',
+        name: 'Jelena Jakovljević',
+        text: ``,
+        url: 'https://www.instagram.com/jenajakovljevich/',
+    },
+
+    {
+        logo: '',
+        name: 'Jelena Trimčević',
+        text: ``,
+        url: 'https://www.instagram.com/jelenatrimcevic/',
+    },
     {
         logo: '',
         name: 'Tijana Lalović',
